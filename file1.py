a0a0e78132e2e9f134ff4cf689e17dbc30fff695
divya-sunkad <divyasunkad24@gmail.com>
2025-11-15 12:10:14 +0530

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,8 @@
+emp_name="Divya"
+emp_Id="275"
+salary="70000"
+address="Hubli"
+print("employee nanme is:",emp_name)
+print("employee id is:",emp_Id)
+print("employee salary is:",salary)
+print("employee adsress is:",address)
